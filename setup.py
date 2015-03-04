b846f38978ac112d0d2a2106331e8d83f9bb4166
Ryan Wallner <ryan.wallner@emc.com>
2015-03-04 11:47:37 -0500

    add supporting files for pip install, not complete do not merge

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name='nvpnsxapi',
+  version='0.1'
+  # TODO
+)
