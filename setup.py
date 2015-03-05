126f39880040aaade56e35a0f668be63c4ac451e
Ryan Wallner <ryan.wallner@emc.com>
2015-03-05 14:34:22 -0500

    initial pip support, untested, do not merge

--- setup.py
+++ setup.py
@@ -10,6 +10,33 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 
 setup(
   name='nvpnsxapi',
-  version='0.1'
-  # TODO
+  version='0.1.0.dev1',
+  description='An NSX / NVP API Binding for Python',
+  long_description=long_description,
+  url='https://github.com/wallnerryan/nsxnvpapi',
+  author='Ryan Wallner',
+  author_email='wallnerryan@gmail.com',
+  license='Apache 2.0',
+  classifiers=[
+
+    'Development Status :: 1 - Alpha',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: API Bindings',
+
+     'License :: OSI Approved :: Apache 2.0 License',
+
+    # Python versions supported 
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+  ],
+  keywords='nsx nvp api python',
+  packages=find_packages(exclude=['tests*']),
+  install_requires = ['python-simplejson', 'python-m2crypto', 'python-pexpect'],
+  data_files=[('confs', ['etc/nvp.conf', 'automation/configs/nvp-config-example.json'])],
+  entry_points={
+    'console_scripts': [
+        'transportnode=automation:transportnode:main',
+    ],
+}
 )
