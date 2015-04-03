4dba5662a2624b691c54acb10c3abc694a7720e8
Ryan Wallner <ryan.wallner@emc.com>
2015-04-03 15:16:17 -0400

    support pip install
    Signed-off-by: Ryan Wallner <ryan.wallner@emc.com>

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
-from codecs import open  # To use a consistent encoding
+import codecs  # To use a consistent encoding
 from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+# Get the long description from the relevant file                               
+with codecs.open('DESCRIPTION.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
   name='nvpnsxapi',
-  version='0.1.0.dev1',
+  version='0.1.3.dev1',
   description='An NSX / NVP API Binding for Python',
   long_description=long_description,
-  url='https://github.com/wallnerryan/nsxnvpapi',
+  url='https://github.com/wallnerryan/nvpnsxapi',
   author='Ryan Wallner',
   author_email='wallnerryan@gmail.com',
   license='Apache 2.0',
   classifiers=[
 
-    'Development Status :: 1 - Pre-Alpha',
+    'Development Status :: 2 - Pre-Alpha',
 
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: API Bindings',
+    'Topic :: Software Development :: Libraries :: Python Modules',
 
-     'License :: OSI Approved :: Apache 2.0 License',
+    'License :: OSI Approved :: Apache Software License',
 
     # Python versions supported 
     'Programming Language :: Python :: 2.6',
@@ -35,7 +33,13 @@ setup(
   install_requires = ['simplejson', 'paramiko','m2crypto',
                       'swigibpy', 'configparser', 'pyOpenSSL',
                       'pexpect'],
-  data_files=[('confs', ['etc/nvp.conf', 'automation/configs/nvp-config-example.json'])],
+  data_files=[('/etc/nvp/', ['etc/nvp.conf']),
+              ('/etc/nvp/configs/',['automation/configs/nvp-config-example.json'])
+  ],
+  package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.json', '*.conf'],
+  },
   entry_points={
     'console_scripts': [
         'transportnode=automation.transportnode:main',
